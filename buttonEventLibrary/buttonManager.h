d8009d800c2656c2ce7bae84d48057c72d42b7e2
amf95 <31721588+amf95@users.noreply.github.com>
2022-02-01 16:44:04 +0200

    Update buttonManager.h
    Encapsulated

--- buttonEventLibrary/buttonManager.h
+++ buttonEventLibrary/buttonManager.h
@@ -11,9 +11,7 @@
 #define BUTTON_LONG_PRESS_LEASE_TIME_THRESHOLD 1500
 #define BUTTON_DOUBLE_CLICK_LEASE_TIME_THRESHOLD 200
 
-
 //#define D_1
-#define D_2 
 
 #ifdef D_1 
 #define DEBUG(x); Serial.println(x);
@@ -21,31 +19,16 @@
 #define DEBUG(x);
 #endif
 
-
-#ifdef D_2 
-#define DEBUG2(x); Serial.println(x);
-#else  
-#define DEBUG2(x);
-#endif
-
 //                    0       1        2          3       4      5          6           7           8
 enum BUTTON_STATES {OPEN, DEBOUNCE, PRESS, LONG_PRESS , STUCK, CLICK , DOUBLE_CLICK, RELEASE, TAKE_ACTION};
 
-typedef struct 
-{
-  byte buttonCurrentState;
-  byte buttonPreviousState;
-  uint64_t timeTracker;
-  byte on;
-  byte off;
-  byte buttonPinNumber;
-}
-buttonTrackers_t;
+struct buttonTrackers_t;//Encapsulated.
 
 typedef buttonTrackers_t * buttonEventManager_t;
 
 buttonEventManager_t buttonManagerInit(uint8_t buttonPinNumber, uint8_t pinDirection, uint8_t buttonOnPhysicalState);
 void onButtonEvent(buttonEventManager_t button,void (*buttonCallbackFun)(void),byte Event);
+uint8_t getButtonEvent(buttonEventManager_t button,byte Event);
 void buttonManagerStateUpdate(buttonEventManager_t button, byte buttonPreviousState, byte buttonCurrentState, uint64_t timeTracker);
 
 #endif
