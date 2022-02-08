20cc525d2533d309a9b19cc3d81a574f94688ceb
amf95 <31721588+amf95@users.noreply.github.com>
2022-02-08 11:29:37 +0200

    Update buttonManager.h

--- buttonEventLibrary/buttonManager.h
+++ buttonEventLibrary/buttonManager.h
@@ -1,7 +1,24 @@
+/*
+**
+** Version: 1.0
+** Auther: Ahmed Fawzy.
+** Created: Mon 31 Jan 2022 04:27:22 PM.
+** Last Modification: Tue 08 Feb 2022 10:53:33 AM.
+** 
+**
+** Description: This library is used to debounce push button click/press and detect other evenets.
+** Events of interest: ( PRESS, LONG_PRESS, CLICK , DOUBLE_CLICK, RELEASE ).
+** Use:
+**  1- Create buttonEventManager_t instance.
+**  2- Init with buttonManagerInit().
+**  3- Use onButtonEvent() and give it button object, callback function and event to listen to.
+**
+*/
+
 #include <Arduino.h>
 
-#ifndef MANAGER
-#define MANAGER
+#ifndef BUTTON_MANAGER
+#define BUTTON_MANAGER
 
 //Configs:
 
@@ -12,6 +29,7 @@
 #define BUTTON_DOUBLE_CLICK_LEASE_TIME_THRESHOLD 200
 
 //#define D_1
+#define D_2 
 
 #ifdef D_1 
 #define DEBUG(x); Serial.println(x);
@@ -19,16 +37,28 @@
 #define DEBUG(x);
 #endif
 
-//                    0       1        2          3       4      5          6           7           8
-enum BUTTON_STATES {OPEN, DEBOUNCE, PRESS, LONG_PRESS , STUCK, CLICK , DOUBLE_CLICK, RELEASE, TAKE_ACTION};
+#ifdef D_2 
+#define DEBUG2(x); Serial.println(x);
+#else  
+#define DEBUG2(x);
+#endif
 
 struct buttonTrackers_t;//Encapsulated.
 
 typedef buttonTrackers_t * buttonEventManager_t;
 
-buttonEventManager_t buttonManagerInit(uint8_t buttonPinNumber, uint8_t pinDirection, uint8_t buttonOnPhysicalState);
-void onButtonEvent(buttonEventManager_t button,void (*buttonCallbackFun)(void),byte Event);
+enum BUTTON_STATES {OPEN, DEBOUNCE, PRESS, LONG_PRESS ,
+STUCK, CLICK , DOUBLE_CLICK, RELEASE, TAKE_ACTION};
+
+buttonEventManager_t buttonManagerInit(uint8_t buttonPinNumber, 
+    uint8_t pinDirection, uint8_t buttonOnPhysicalState);
+
+void onButtonEvent(buttonEventManager_t button,
+    void (*buttonCallbackFun)(void),byte Event);
+    
 uint8_t getButtonEvent(buttonEventManager_t button,byte Event);
-void buttonManagerStateUpdate(buttonEventManager_t button, byte buttonPreviousState, byte buttonCurrentState, uint64_t timeTracker);
+
+void buttonManagerStateUpdate(buttonEventManager_t button,
+    byte buttonPreviousState, byte buttonCurrentState, uint64_t timeTracker);
 
 #endif
